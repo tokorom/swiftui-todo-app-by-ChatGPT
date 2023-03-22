4af213d1beca00593720e52ef5cf8d115c0813d3
tokorom <tokorom@gmail.com>
2023-03-22 18:33:10 +0900

    ChatGPT A3

--- TodoAppWithChatGPT/TaskStore.swift
+++ TodoAppWithChatGPT/TaskStore.swift
@@ -2,7 +2,17 @@ import SwiftUI
 import Combine
 
 class TaskStore: ObservableObject {
-    @Published var tasks: [Task] = []
+    @Published var tasks: [Task] {
+        didSet {
+            saveTasks()
+        }
+    }
+
+    private let tasksKey = "tasksKey"
+
+    init() {
+        tasks = loadTasks()
+    }
 
     func addTask(title: String) {
         tasks.append(Task(title: title))
@@ -14,5 +24,19 @@ class TaskStore: ObservableObject {
 
     func toggleTaskCompletion(at index: Int) {
         tasks[index].isCompleted.toggle()
+    }
+
+    private func saveTasks() {
+        if let encodedTasks = try? JSONEncoder().encode(tasks) {
+            UserDefaults.standard.set(encodedTasks, forKey: tasksKey)
+        }
+    }
+
+    private func loadTasks() -> [Task] {
+        if let tasksData = UserDefaults.standard.data(forKey: tasksKey),
+           let decodedTasks = try? JSONDecoder().decode([Task].self, from: tasksData) {
+            return decodedTasks
+        }
+        return []
     }
 }
