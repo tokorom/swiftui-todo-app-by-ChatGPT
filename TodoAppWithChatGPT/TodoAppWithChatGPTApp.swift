8117caa0e935fc35f2d80337f590fd367a643b79
tokorom <tokorom@gmail.com>
2023-03-22 18:12:58 +0900

    First commit

---
+++ TodoAppWithChatGPT/TodoAppWithChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppWithChatGPTApp.swift
+//  TodoAppWithChatGPT
+//
+//  Created by Yuta Tokoro on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppWithChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
